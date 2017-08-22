4067919fc218cf2bc6e7e08a1ce8f2ef8d7c4615
pigpigever <1690384523@qq.com>
2017-08-22 14:41:29 +0800

    新建Easy文件夹，对题目分类

---
+++ Easy/Assign Cookies/task.js
@@ -0,0 +1,23 @@
+/**
+ * @param {number[]} g
+ * @param {number[]} s
+ * @return {number}
+ */
+var findContentChildren = function(g, s) {
+    let sum = 0
+    g.sort((a,b)=>(a-b))
+    s.sort((a,b)=>(a-b))
+    for(let i=0;i<g.length;i++){
+        for(let j=0;j<s.length;j++){
+            if(s[j] >= g[i]){
+                s.splice(j,1)
+                sum ++
+                break
+            }
+        }
+        if(s.length === 0){
+            return sum
+        }
+    }
+    return sum
+};
