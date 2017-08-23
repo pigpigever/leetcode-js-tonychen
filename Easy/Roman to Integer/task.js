f484758284430a904a964fcf347d0f6c113a2858
pigpigever <1690384523@qq.com>
2017-08-23 23:44:02 +0800

    Roman to Integer

---
+++ Easy/Roman to Integer/task.js
@@ -0,0 +1,40 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var romanToInt = function(s) {
+    var nums= []
+    for(var i=0;i<s.length;i++){
+        switch (s[i]){
+            case 'M':
+                nums[i]=1000;
+                break;
+            case 'D':
+                nums[i]=500;
+                break;
+            case 'C':
+                nums[i]=100;
+                break;
+            case 'L':
+                nums[i]=50;
+                break;
+            case 'X' :
+                nums[i]=10;
+                break;
+            case 'V':
+                nums[i]=5;
+                break;
+            case 'I':
+                nums[i]=1;
+                break;
+        }
+    }
+    var sum=0;
+    for(var i=0;i<nums.length-1;i++){
+        if(nums[i]<nums[i+1])
+            sum-=nums[i];
+        else
+            sum+=nums[i];
+    }
+    return sum+nums[nums.length-1];
+};
