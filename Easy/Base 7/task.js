301a29b08659734b3f4eff33ab0cc48dc809af22
pigpigever <1690384523@qq.com>
2017-08-31 21:58:42 +0800

    今日刷题

---
+++ Easy/Base 7/task.js
@@ -0,0 +1,14 @@
+/**
+ * @param {number} num
+ * @return {string}
+ */
+var convertToBase7 = function(num) {
+    let result = [],temp = num
+    num = num >= 0 ? num : -num
+    if(!num) return '0'
+    while(num!==0){
+        result.unshift(num%7)
+        num = Math.floor(num / 7)
+    }
+    return temp >= 0 ? result.join('') : '-'+result.join('')
+};
---
+++ Easy/Construct String from Binary Tree/task.js
@@ -0,0 +1,31 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} t
+ * @return {string}
+ */
+
+var tree2str = function (t) {
+    let result = '',
+        stack = [];
+    stack.push(t);
+    while(stack.length){
+        let i = stack.length;
+        while(i--){
+            let output = stack.shift();
+            console.log(output);
+            if(output.left !== null){
+                stack.push(output.left);
+            }
+            if(output.right !== null){
+                stack.push(output.right);
+            }
+        }
+    }
+    return result
+};
