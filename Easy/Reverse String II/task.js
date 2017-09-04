fa0bc8ba7106682698f67b49f004bd7d9eca4e79
pigpigever <1690384523@qq.com>
2017-09-04 11:48:37 +0800

    Reverse String II

--- Easy/Reverse String II/task.js
+++ Easy/Reverse String II/task.js
@@ -3,13 +3,15 @@
  * @param {number} k
  * @return {string}
  */
-var reverseStr = function(s, k) {
-    let sum = [],length = s.length,i=0
-    while(s.length !== 0){
-        sum.push(s.split('').slice(0,2*k).join(''))
-        s = s.split('').slice(2*k).join('')
+var reverseStr = function (s, k) {
+    let sum = [],
+        length = s.length,
+        i = 0
+    while (s.length !== 0) {
+        sum.push(s.split('').slice(0, 2 * k).join(''))
+        s = s.split('').slice(2 * k).join('')
     }
-    return sum.map(item=>{
-        return item.split('').slice(0,k).reverse().join('')+item.split('').slice(k).join('')
+    return sum.map(item => {
+        return item.split('').slice(0, k).reverse().join('') + item.split('').slice(k).join('')
     }).join('')
 };
