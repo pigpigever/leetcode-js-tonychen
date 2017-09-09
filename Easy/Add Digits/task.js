13760444b173859da5355119a6d9b032b4f9fc4e
Pigpigever <1690384523@qq.com>
2017-09-09 12:26:47 +0800

    Add Two Numbers

--- Easy/Add Digits/task.js
+++ Easy/Add Digits/task.js
@@ -2,16 +2,14 @@
  * @param {number} num
  * @return {number}
  */
-var addDigits = function(num) {
-    var temp=num.toString();
-    while(temp.length!=1)
-    {
-        var sum=0;
-        for(var i=0;i<temp.length;i++)
-        {
-            sum+=parseInt(temp[i]);
+var addDigits = function (num) {
+    var temp = num.toString();
+    while (temp.length != 1) {
+        var sum = 0;
+        for (var i = 0; i < temp.length; i++) {
+            sum += parseInt(temp[i]);
         }
-        temp=sum+"";
+        temp = sum + "";
     }
     return parseInt(temp);
 };
