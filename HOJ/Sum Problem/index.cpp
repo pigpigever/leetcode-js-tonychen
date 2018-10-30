992c263e9b3dab83ef05673b409f918a8d3b38d9
pigpigever <1690384523@qq.com>
2018-10-30 15:50:07 +0800

    update probleset

---
+++ HOJ/Sum Problem/index.cpp
@@ -0,0 +1,34 @@
+/*
+ *
+	Problem Description
+	Hey, welcome to HDOJ(Hangzhou Dianzi University Online Judge).
+
+	In this problem, your task is to calculate SUM(n) = 1 + 2 + 3 + ... + n.
+	
+	Input
+	The input will consist of a series of integers n, one integer per line.
+	
+	Output
+	For each case, output SUM(n) in one line, followed by a blank line. You may assume the result will be in the range of 32-bit signed integer.
+	
+	Sample Input
+	1
+	100
+	
+	Sample Output
+	1
+
+	5050
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int main () {
+	long int start = 1, end;
+	while (cin>>end) {
+		cout<<  (start + end) * end / 2<<endl<<endl;
+	}
+	return 0;
+}
