e61ad8a40d096f4c9cff24b3329d4405e362e733
pigpigever <1690384523@qq.com>
2019-07-13 16:48:33 +0800

    83. Remove Duplicates from Sorted List

---
+++ LeetCode/Easy/83. Remove Duplicates from Sorted List/deleteDuplicates.js
@@ -0,0 +1,36 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var deleteDuplicates = function(head) {
+    let pre = head, tmp = head
+    tmp = tmp.next
+
+    while (tmp) {
+        if (tmp.val === pre.val) {
+            pre.next = tmp.next
+        } else {
+           pre = tmp
+        }
+        tmp = tmp.next
+    }
+    return head
+};
+
+// 给定一个排序链表，删除所有重复的元素，使得每个元素只出现一次。
+
+// 示例 1:
+
+// 输入: 1->1->2
+// 输出: 1->2
+// 示例 2:
+
+// 输入: 1->1->2->3->3
+// 输出: 1->2->3
