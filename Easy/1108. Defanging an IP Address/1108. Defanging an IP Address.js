9196ad47aa88a0a8ffdf2d575a8f205b5005ba2e
tonychen <1690384523@qq.com>
2019-07-19 16:56:06 +0800

    IP地址无效化

---
+++ Easy/1108. Defanging an IP Address/1108. Defanging an IP Address.js
@@ -0,0 +1,15 @@
+/**
+ * @param {string} address
+ * @return {string}
+ */
+var defangIPaddr = function(address) {
+    let str = ''
+    for (let i = 0; i < address.length; i++) {
+        if (address[i] === '.') {
+            str += '[.]'
+        } else {
+            str += address[i]
+        }
+    }
+    return str
+};
