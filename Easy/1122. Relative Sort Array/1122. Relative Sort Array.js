56d547acbc72d526b3bc7d0c3a49003e0985e4dd
tonychen <1690384523@qq.com>
2019-07-19 20:17:40 +0800

    更新解法

--- Easy/1122. Relative Sort Array/1122. Relative Sort Array.js
+++ Easy/1122. Relative Sort Array/1122. Relative Sort Array.js
@@ -4,16 +4,18 @@
  * @return {number[]}
  */
 var relativeSortArray = function(arr1, arr2) {
-    let result = []
-
-    while (arr2.length) {
-        let i = arr1.length
-        let target = arr2.pop()
-        while (i--) {
-            if (arr1[i] === target) {
-                result.unshift(target)
-                arr1.splice(i, 1)
-            }
+    let result = [], i = 0, target
+    while (true) {
+        if (i === 0) {
+            i = arr1.length
+            target = arr2.pop()
+        }
+        if (arr1[--i] === target) {
+            result.unshift(target)
+            arr1.splice(i, 1)
+        }
+        if (!arr2.length && !i) {
+            break
         }
     }
     return result.concat(arr1.sort((a, b) => a - b))
