4e7170f4749465e9b6c6ba1464006cb0f8e192a6
tonychen <1690384523@qq.com>
2019-07-22 00:00:19 +0800

    精简代码

--- Easy/219. Contains Duplicate II/219. Contains Duplicate II.js
+++ Easy/219. Contains Duplicate II/219. Contains Duplicate II.js
@@ -7,16 +7,11 @@ var containsNearbyDuplicate = function(nums, k) {
     let map = {}
     for (let i = 0; i < nums.length; i++) {
         if (map[nums[i]] !== undefined) {
-            if (i - map[nums[i]] === k) {
+            if (i - map[nums[i]] <= k) {
                 return true
-            } else if (i - map[nums[i]] > k) {
-                map[nums[i]] = i
             }
-        } else {
-            map[nums[i]] = i
         }
+        map[nums[i]] = i
     }
     return false
 };
-
-console.log(containsNearbyDuplicate([1,0,1,1], 1))
