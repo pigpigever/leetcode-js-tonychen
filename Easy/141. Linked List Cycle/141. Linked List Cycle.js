6777363bf39b1df8e660d53116ac5dc4339d1655
tonychen <1690384523@qq.com>
2019-07-23 14:52:54 +0800

    环形链表

---
+++ Easy/141. Linked List Cycle/141. Linked List Cycle.js
@@ -0,0 +1,26 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+
+/**
+ * @param {ListNode} head
+ * @return {boolean} 
+*/
+var hasCycle = function(head) {
+    if (!head || !head.next) {
+        return false
+    }
+    let slow = head, fast = head.next
+    while (slow) {
+        if (slow.val === fast.val) {
+            return true
+        }
+        slow = slow.next
+        fast = fast.next && fast.next.next
+    }
+    return false
+};
