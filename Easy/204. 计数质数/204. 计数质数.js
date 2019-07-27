610a8172f74ade2deb7485cbe2315c74eeb1ffc9
tonychen <1690384523@qq.com>
2019-07-27 18:37:05 +0800

    计数质数

--- Easy/204. 计数质数/204. 计数质数.js
+++ Easy/204. 计数质数/204. 计数质数.js
@@ -3,19 +3,20 @@
  * @return {number}
  */
 var countPrimes = function(n) {
-    let ans = 0
+    let ans = 0, nums = new Array(n).fill(true)
+    for (let i = 2; i * i <= n; i++) {
+        if (nums[i]) {
+            let k = 2
+            while (i * k <= n) {
+                nums[i * k] = false
+                k++
+            }
+        }
+    }
     for (let i = 2; i < n; i++) {
-        if (isPrime(i)) {
+        if (nums[i]) {
             ans++
         }
     }
     return ans
-    function isPrime (num) {
-        for (let i = 2; i * i <= num; i++) {
-            if (num % i === 0) {
-                return false
-            }
-        }
-        return true
-    }
 };
