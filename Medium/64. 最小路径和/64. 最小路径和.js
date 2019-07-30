a59151be544772ee5fc658068725b713e2420440
tonychen <1690384523@qq.com>
2019-07-30 10:15:57 +0800

    优化

--- Medium/64. 最小路径和/64. 最小路径和.js
+++ Medium/64. 最小路径和/64. 最小路径和.js
@@ -9,8 +9,6 @@ var minPathSum = function(grid) {
             if (j === 0) {
                 if (i > 0) {
                     grid[i][j] = grid[i - 1][j] + grid[i][j]
-                } else {
-                    grid[i][j] = grid[i][j]
                 }
             } else {
                 if (i > 0) {
