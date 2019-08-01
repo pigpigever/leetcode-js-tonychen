acc2a1c780b6a4431a10ed9aa37291bc23f22bb8
tonychen <1690384523@qq.com>
2019-08-01 13:47:59 +0800

    优化

--- Easy/788. 旋转数字/788. 旋转数字.js
+++ Easy/788. 旋转数字/788. 旋转数字.js
@@ -5,23 +5,9 @@
 var rotatedDigits = function(N) {
     let ans = 0
     for (let i = 1; i <= N; i++) {
-        if (isGoodNum(i)) {
+        if (/^(?![018]+$)[0125689]+$/.test(i)) {
             ans++
         }
     }
     return ans
-    function isGoodNum (num) {
-        let flag = false
-        while (num > 0) {
-            let tmp = num % 10
-            if ([0,1,2,5,6,8,9].every((item) => item !== tmp)) {
-                return false
-            }
-            if ([2,5,6,9].some((item) => item === tmp) && !flag) {
-                flag = true
-            } 
-            num = Math.floor(num / 10)
-        }
-        return flag
-    }
 };
