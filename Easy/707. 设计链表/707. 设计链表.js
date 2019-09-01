3d2b3667ad8995b8633b8e0518c016ecd503b877
tonychen <tonychen@xiaoe-tech.com>
2019-09-01 13:35:54 +0800

    设计链表

---
+++ Easy/707. 设计链表/707. 设计链表.js
@@ -0,0 +1,68 @@
+/**
+ * Initialize your data structure here.
+ */
+var MyLinkedList = function() {
+    this.list = []
+};
+
+var Node = function (val) {
+    this.val = val
+}
+
+/**
+ * Get the value of the index-th node in the linked list. If the index is invalid, return -1. 
+ * @param {number} index
+ * @return {number}
+ */
+MyLinkedList.prototype.get = function(index) {
+    if (index >= 0 && index < this.list.length) {
+        return this.list[index].val
+    }
+    return -1
+};
+
+/**
+ * Add a node of value val before the first element of the linked list. After the insertion, the new node will be the first node of the linked list. 
+ * @param {number} val
+ * @return {void}
+ */
+MyLinkedList.prototype.addAtHead = function(val) {
+    let node = new Node(val)
+    this.list.unshift(node)
+};
+
+/**
+ * Append a node of value val to the last element of the linked list. 
+ * @param {number} val
+ * @return {void}
+ */
+MyLinkedList.prototype.addAtTail = function(val) {
+    let node = new Node(val)
+    this.list.push(node)
+};
+
+/**
+ * Add a node of value val before the index-th node in the linked list. If index equals to the length of linked list, the node will be appended to the end of linked list. If index is greater than the length, the node will not be inserted. 
+ * @param {number} index 
+ * @param {number} val
+ * @return {void}
+ */
+MyLinkedList.prototype.addAtIndex = function(index, val) {
+    if (index > this.list.length) {
+        return
+    }
+    let node = new Node(val)
+    this.list.splice(index, 0, node)
+};
+
+/**
+ * Delete the index-th node in the linked list, if the index is valid. 
+ * @param {number} index
+ * @return {void}
+ */
+MyLinkedList.prototype.deleteAtIndex = function(index) {
+    if (index < 0) {
+        return
+    }
+    this.list.splice(index, 1)
+};
