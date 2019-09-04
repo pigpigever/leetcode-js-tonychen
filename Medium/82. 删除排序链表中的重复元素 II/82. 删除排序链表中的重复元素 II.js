91e7cbc3a1e2fab164efabee8331da42d983748c
tonychen <tonychen@xiaoe-tech.com>
2019-09-04 23:24:52 +0800

    删除排序链表中的重复元素

---
+++ Medium/82. 删除排序链表中的重复元素 II/82. 删除排序链表中的重复元素 II.js
@@ -0,0 +1,30 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var deleteDuplicates = function(head) {
+    let firstPre = '', flag = false
+    let primtive = new ListNode(), target = primtive, next = head, pre = '', 
+    primtive.next = head
+    while (target) {
+        if (target.val === (next && next.val)) {
+            firstPre = pre
+            flag = true
+        } else if (flag) {
+            firstPre.next = next
+            target = firstPre
+            flag = false
+        }
+        pre = target
+        target = target.next
+        next = next && next.next
+    }
+    return primtive.next
+};
