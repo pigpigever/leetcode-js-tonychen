c657884e646f463a350bd2fcfc5c94f056908112
tonychen <tonychen@xiaoe-tech.com>
2019-11-14 22:32:55 +0800

    Update

--- Easy/Reverse Integer/task.js
+++ Easy/Reverse Integer/task.js
@@ -3,18 +3,15 @@
  * @return {number}
  */
 var reverse = function (x) {
-	let temp = ''
-	
-	if (x > 0) {
-		temp = String(x).split('').reverse().join('')
-	} else {
-		temp = String(-x).split('').reverse().join('')
+	let ans = 0, tmp = 0
+	const INT_MAX = Math.pow(2, 31) - 1, INT_MIN = -Math.pow(2, 31)
+	while (x) {
+		tmp = x % 10
+		x = (x / 10) >> 0
+		ans = ans * 10 + tmp
+		if (ans < INT_MIN || ans > INT_MAX) {
+			return 0
+		}
 	}
-	let result = x > 0 ? Number(temp) : -1 * Number(temp)
-	if (result > Math.pow(2, 31) - 1 || result < -Math.pow(2, 31)) {
-		return 0
-	}
-	return result
-};
-
-console.log(reverse(1534236469))
+	return ans
+}
