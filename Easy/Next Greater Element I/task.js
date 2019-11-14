767c1d1101061a8749ab213709d57bec33563d87
tonychen <tonychen@xiaoe-tech.com>
2019-11-14 22:32:55 +0800

    Update

--- Easy/Next Greater Element I/task.js
+++ Easy/Next Greater Element I/task.js
@@ -1,31 +1,24 @@
 /**
-     * @param {number[]} findNums
-     * @param {number[]} nums
-     * @return {number[]}
-     */
-    var nextGreaterElement = function(findNums, nums) {
-        var result=[];
-        for(var i=0;i<findNums.length;i++){
-            var tag=undefined,isTrue=false;
-            for(var j=0;j<nums.length;j++){
-                if(findNums[i]==nums[j]){
-                    tag=j;
-                    break;
-                }
-            }
-            if(tag!==undefined){
-                for(var j=tag+1;j<nums.length;j++){
-                    if(findNums[i]<nums[j]){
-                        result.push(nums[j]);
-                        isTrue=true;
-                        break;
-                    }
-                }
-                if(!isTrue){result.push(-1)};
-            }
-            else{
-                result.push(-1);
+ * @param {number[]} nums1
+ * @param {number[]} nums2
+ * @return {number[]}
+ */
+var nextGreaterElement = function(nums1, nums2) {
+    const map = new Map()
+    const ans = []
+    for (let i = 0; i < nums2.length; i++) {
+        map.set(nums2[i], i)
+    }
+    for (const num of nums1) {
+        const targetIndex = map.get(num)
+        let flag = 0
+        for (let i = targetIndex + 1; i < nums2.length; i++) {
+            if (num < nums2[i]) {
+                flag = i
+                break
             }
         }
-        return result;
-    };
+        ans.push(flag ? nums2[flag] : -1)
+    }
+    return ans
+};
