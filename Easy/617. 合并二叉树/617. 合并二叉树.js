325f79667f0789b81474aa4a9d87ebd826cd4a7f
tonychen <tonychen@xiaoe-tech.com>
2019-12-19 03:15:57 +0800

    update

--- Easy/617. 合并二叉树/617. 合并二叉树.js
+++ Easy/617. 合并二叉树/617. 合并二叉树.js
@@ -11,28 +11,14 @@
  * @return {TreeNode}
  */
 var mergeTrees = function(t1, t2) {
-    if (t1 || t2) {
-        const tree = new TreeNode()
-        dfs(tree, t1, t2)
-        return tree
+    if (!t1) {
+        return t2
     }
-    return null
-
-    function dfs (tree, t1, t2) {
-        if (t1 || t2) {
-            const first = t1 ? t1.val : 0
-            const second = t2 ? t2.val : 0
-            const left1 = t1 ? t1.left : null
-            const left2 = t2 ? t2.left : null
-            const right1 = t1 ? t1.right : null
-            const right2 = t2 ? t2.right : null
-            tree.val = first + second
-            if (left1 || left2) {
-                dfs(tree.left = new TreeNode(), left1, left2)
-            }
-            if (right1 || right2) {
-                dfs(tree.right = new TreeNode(), right1, right2)
-            }
-        }
+    if (!t2) {
+        return t1
     }
+    t1.val += t2.val
+    t1.left = mergeTrees(t1.left, t2.left)
+    t1.right = mergeTrees(t1.right, t2.right)
+    return t1
 };
