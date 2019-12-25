7d65c74b6f3dddb64208dc8ca2660e65aa3889aa
tonychen <tonychen@xiaoe-tech.com>
2019-12-25 16:47:27 +0800

    feat: 二叉树的前序遍历

---
+++ Medium/144. 二叉树的前序遍历/144. 二叉树的前序遍历.js
@@ -0,0 +1,17 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number[]}
+ */
+var preorderTraversal = function(root) {
+    if (!root) {
+        return []
+    }
+    return [root.val].concat(preorderTraversal(root.left)).concat(preorderTraversal(root.right))
+};
