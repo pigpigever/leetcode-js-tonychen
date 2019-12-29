26a20d1550b7088901a8164a7809bae582a59565
tonychen <tonychen@xiaoe-tech.com>
2019-12-29 19:49:36 +0800

    feat: 145. 二叉树的后序遍历

---
+++ Hard/145. 二叉树的后序遍历/145. 二叉树的后序遍历.js
@@ -0,0 +1,19 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number[]}
+ */
+var postorderTraversal = function(root) {
+    if (!root) {
+        return []
+    }
+    return postorderTraversal(root.left)
+        .concat(postorderTraversal(root.right))
+        .concat([root.val])
+};
