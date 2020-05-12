30ce59e53af3a583c4a1c6adb04d8c6f4a950992
tonychen <tonychen@xiaoe-tech.com>
2020-05-12 23:05:40 +0800

    add 最小栈

--- Easy/155. Min Stack/155. Min Stack.js
+++ Easy/155. Min Stack/155. Min Stack.js
@@ -1,55 +1,22 @@
-/**
- * initialize your data structure here.
- */
-var MinStack = function() {
-    this.ele = []
-    this.min = Number.MAX_SAFE_INTEGER
+const MinStack = function() {
+    this.stack = [];
+    this.minStack = [Infinity];
 };
 
-/** 
- * @param {number} x
- * @return {void}
- */
 MinStack.prototype.push = function(x) {
-    if (this.min > x) {
-        this.min = x
-    }
-    this.ele.push(x)
+    this.stack.unshift(x);
+    this.minStack.unshift(Math.min(this.minStack[0], x));
 };
 
-/**
- * @return {void}
- */
 MinStack.prototype.pop = function() {
-    if (this.min === this.ele.pop()) {
-        this.min = Number.MAX_SAFE_INTEGER   
-    }
-    for (let i = 0; i < this.ele.length; i++) {
-        if (this.ele[i] < this.min) {
-            this.min = this.ele[i]
-        }
-    }
+    this.stack.shift();
+    this.minStack.shift();
 };
 
-/**
- * @return {number}
- */
 MinStack.prototype.top = function() {
-    return this.ele[this.ele.length - 1]
+    return this.stack[this.stack.length - 1];
 };
 
-/**
- * @return {number}
- */
 MinStack.prototype.getMin = function() {
-    return this.min
+    return this.minStack[this.minStack.length - 1];
 };
-
-/** 
- * Your MinStack object will be instantiated and called as such:
- * var obj = new MinStack()
- * obj.push(x)
- * obj.pop()
- * var param_3 = obj.top()
- * var param_4 = obj.getMin()
- */
