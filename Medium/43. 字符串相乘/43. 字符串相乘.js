c777184716c45dd9e3c2b342ced4daa8b6c94f70
pigpigever <pigpigever@gmail.com>
2025-11-10 14:38:07 +0800

    feat: 字符串相乘

--- Medium/43. 字符串相乘/43. 字符串相乘.js
+++ Medium/43. 字符串相乘/43. 字符串相乘.js
@@ -4,5 +4,52 @@
  * @return {string}
  */
 const multiply = function(num1, num2) {
+  const arr = []
+  let carry = 0
+  let count = 0
 
+  const trimLeadingZeros = str => {
+    let i = 0;
+    while (i < str.length - 1 && str[i] === '0') i++;
+    return str.slice(i);
+  };
+
+  const add = (num1, num2) => {
+    let i = num1.length - 1
+    let j = num2.length - 1
+    let str = ''
+    let carry = 0
+    while (i >= 0 || j >= 0){
+      const a = i >= 0 ? Number(num1[i]) : 0
+      const b = j >= 0 ? Number(num2[j]) : 0
+      const value = (a + b + carry) % 10
+      carry = Math.floor((a + b + carry) / 10)
+      str = value + str
+      i--
+      j--
+    }
+    if (carry) {
+      str = carry + str
+    }
+    return str.replace(/^0/g, '')
+  }
+
+  for (let i = num2.length - 1; i >= 0; i--) {
+    let str = ''
+    for (let j = num1.length - 1; j >= 0; j--) {
+      const value = (num1[j] * num2[i] + carry) % 10
+      carry = Math.floor((num1[j] * num2[i] + carry) / 10)
+      str = value + str
+    }
+    if (carry) {
+      str = carry + str
+      carry = 0
+    }
+    const length = str.length + count
+    arr.push(str.padEnd(length, '0'))
+    count++
+  }
+  return trimLeadingZeros(
+    arr.reduce((result, curr) => add(result, curr))
+  )
 }
